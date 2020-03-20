46b5df6d956ac96bd4b22a195b295fef0ebbb7c5
Omar <omarcr.96@gmail.com>
2020-03-19 23:47:05 -0600

    Creacionde la ruta usuario donde consultamos el API de JSONPlaceholder y lo mostramos como parte de nuestra API Rest

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const router = Router();
+const fetch = require("node-fetch");
 
-router.get("/usuario", (req, res) => {
-  // const usuario = {
-  //     "nombre" : "Omar Rendón",
-  //     "edad": 24,
-  // }
-  
+router.get("/usuario", async (req, res) => {
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/users"
+  )
+  const usuarios = await response.json();
+    res.json(usuarios);
 });
 
 module.exports = router;
